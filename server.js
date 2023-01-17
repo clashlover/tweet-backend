e7e070b21711f0d585b463c9e50ac569c30db05a
clashlover <clashlover927@gmail.com>
2023-01-17 16:36:04 +0530

    first upload

---
+++ server.js
@@ -0,0 +1,45 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://localhost:27017/tweetDB");
+
+const tweetSchema = {
+  tweet: String,
+};
+
+const Tweet = new mongoose.model("Tweet", tweetSchema);
+
+app.get("/", (req, res) => {
+  Tweet.find({}, (err, tweets) => {
+    if (err) throw err;
+    res.json(tweets.reverse());
+  });
+});
+
+app.post("/", (req, res) => {
+  const { tweet } = req.body;
+  const newTweet = new Tweet({ tweet: tweet });
+  newTweet.save();
+  res.status(200).json("Success");
+});
+
+app.delete("/", (req, res) => {
+  const { id } = req.body;
+
+  Tweet.findOneandRemove({ _id: id }, (err) => {
+    if (err) return err;
+    res.json(200).json("Deleted");
+  });
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is running fine");
+});
